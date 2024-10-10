eb60b52492ea88149566904052bb8284deb3b0ec
Calet Devia <calebtius@gmail.com>
2024-10-10 08:28:35 -0500

    Add the header section and the hamburger icon too

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    const navList = document.querySelector('.header__list');
+    // This toggles the display property between 'none' and 'block'
+    navList.style.display = navList.style.display === 'none' ? 'block' : 'none';
+}
