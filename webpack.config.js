4af6142801c2ee2c86453859e4e89df8d2a9f881
Calet Devia <calebtius@gmail.com>
2024-10-22 14:51:38 -0500

    Fixed the name of the filename in the configuration of the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[name][ext]",
+          filename: "image/[name][ext]",
         },
       },
     ],
