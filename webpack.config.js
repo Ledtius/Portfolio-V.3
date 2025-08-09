f827e5f5494c5034f4023ac95f7e2af5b7b3e4c3
Calet Devia <calebtius@gmail.com>
2025-08-08 20:55:16 -0500

    changed config v2

--- webpack.config.js
+++ webpack.config.js
@@ -8,54 +8,46 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: "./js/menu.js",
   output: {
-    filename: "bundle.[contenthash].js", // hash en JS
+    filename: "bundle.[contenthash].js", // Hash en JS
     path: path.resolve(__dirname, "dist"),
-    clean: true, // limpia dist antes de cada build
-    assetModuleFilename: "image/[name].[contenthash][ext]", // por si se usa asset/resource global
+    clean: true, // Limpia dist antes de cada build
   },
   mode: "production",
   module: {
     rules: [
-      // Para procesar HTML y detectar imágenes
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "image/[name].[contenthash][ext]", // Hash en imágenes
+        },
+      },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: "html-loader", // 👈 Necesario para procesar imágenes en HTML
         options: {
           sources: {
             list: [
-              "...", // incluye las reglas por defecto
+              // Procesa atributos src, srcset, etc.
+              "...",
               {
                 tag: "img",
                 attribute: "src",
-                type: "src",
-              },
-              {
-                tag: "link",
-                attribute: "href",
                 type: "src",
               },
             ],
           },
         },
       },
-      // Para CSS
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      // Para imágenes y otros assets
-      {
-        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "image/[name].[contenthash][ext]",
-        },
-      },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "styles.[contenthash].css", // hash en CSS
+      filename: "styles.[contenthash].css", // Hash en CSS
     }),
     new HtmlWebpackPlugin({
       template: "./index.html",
@@ -63,7 +55,7 @@ module.exports = {
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: "_headers", to: "." }, // copia headers para Netlify
+        { from: "_headers", to: "." }, // Copia headers si existen
       ],
     }),
   ],
